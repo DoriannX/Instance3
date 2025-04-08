e315ce7508f11886bfe333a33905fc3b15ed32be
Doriann <theodor.xav@gmail.com>
2025-04-08 11:52:06 +0200

    fix

---
+++ Assets/Scripts/Player/SimpleMoveTest.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class SimpleMoveTest : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 5f;
+        private Transform playerTransform;
+
+        private void Awake()
+        {
+            playerTransform = transform;
+        }
+
+        private void Update()
+        {
+            Vector3 _moveDirection = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                _moveDirection += Vector3.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _moveDirection += Vector3.back;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                _moveDirection += Vector3.left;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                _moveDirection += Vector3.right;
+            }
+            playerTransform.position += _moveDirection.normalized * (Time.deltaTime * moveSpeed);
+        }
+    }
+}
