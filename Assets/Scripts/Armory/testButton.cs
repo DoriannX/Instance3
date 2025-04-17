e0eab2424e6ff4e1670988133060f9b8d1eeac70
theo-bsl <t.basile@lahorde.school>
2025-04-17 12:28:01 +0200

    add sprite

---
+++ Assets/Scripts/Armory/testButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Armory
+{
+    public class testButton : MonoBehaviour
+    {
+        public void OnButtonClick(int i)
+        {
+            Debug.Log(i);
+        }
+    }
+}
